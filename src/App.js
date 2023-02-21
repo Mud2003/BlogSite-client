9fc5ecb8c9ede98303982e28a8e48b3e447d3096
Muditha Madusith <108977790+Mud2003@users.noreply.github.com>
2023-02-21 22:08:25 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href="" className="logo">MyBlog</a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+
+      <div className="post">
+        <div className="image">
+        <img src="https://techcrunch.com/wp-content/uploads/2023/02/Zoox-making-right-hand-turn-on-open-public-roads-in-Foster-City-California.jpg?w=990&crop=1" alt=""/>
+        </div>
+        <div className="texts">
+        <h2>Tesla battles with union organizers, Zoox hits </h2>
+        <p>EV charging has improved over the years, of electric vehicles the Biden Administration wants to see on the road in the next few years. The administrations $7.5 billion EV charging.</p>
+        </div>
+      </div>
+      
+      <div className="post">
+        <div className="image">
+        <img src="https://techcrunch.com/wp-content/uploads/2023/02/Zoox-making-right-hand-turn-on-open-public-roads-in-Foster-City-California.jpg?w=990&crop=1" alt=""/>
+        </div>
+        <div className="texts">
+        <h2>Tesla battles with union organizers, Zoox hits </h2>
+        <p>EV charging has improved over the years, of electric vehicles the Biden Administration wants to see on the road in the next few years. The administrations $7.5 billion EV charging.</p>
+        </div>
+      </div>
+
+      <div className="post">
+        <div className="image">
+        <img src="https://techcrunch.com/wp-content/uploads/2023/02/Zoox-making-right-hand-turn-on-open-public-roads-in-Foster-City-California.jpg?w=990&crop=1" alt=""/>
+        </div>
+        <div className="texts">
+        <h2>Tesla battles with union organizers, Zoox hits </h2>
+        <p>EV charging has improved over the years, of electric vehicles the Biden Administration wants to see on the road in the next few years. The administrations $7.5 billion EV charging.</p>
+        </div>
+      </div>
+
+    </main>
+  );
+}
+
+export default App;
