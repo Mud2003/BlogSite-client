99fe915c9676fc030678fcfd6e5172cc1ab15ef2
Muditha Madusith <108977790+Mud2003@users.noreply.github.com>
2023-02-22 18:08:29 +0530

    Update RegisterPage.js
    Authentications

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,9 +1,33 @@
+import { useState } from "react";
+
 export default function RegisterPage() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function register(ev) {
+    ev.preventDefault();
+    fetch('http://localhost:4000/register', {
+      method: 'POST',
+      body: JSON.stringify({username,password}),
+      headers: {'Content-Type':'application/json'}
+    })
+  }
+
     return (
-      <form className="register">
+      <form className="register" onSubmit={register}>
+
         <h1>Register</h1>
-        <input type="text" placeholder="User Name"/>
-        <input type="password" placeholder="Password"/>
+
+        <input type="text" 
+        placeholder="User Name" 
+        value={username}
+        onChange={ev => setUsername(ev.target.value)}/>
+
+        <input type="password" 
+        placeholder="Password"
+        value={password}
+        onChange={ev => setPassword(ev.target.value)}/>
+
         <button>Register</button>
       </form>
     );
