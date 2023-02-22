8c2dc262687d98662b4a876ccf95ba6056d71563
Muditha Madusith <108977790+Mud2003@users.noreply.github.com>
2023-02-22 15:10:55 +0530

    Update Post.js

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,12 +2,12 @@ export default function Post() {
     return (
         <div className="post">
         <div className="image">
-        
+        <img src="https://techcrunch.com/wp-content/uploads/2023/02/Zoox-making-right-hand-turn-on-open-public-roads-in-Foster-City-California.jpg?w=990&crop=1" alt=""/>
         </div>
         <div className="texts">
         <h2>Tesla battles with union organizers, Zoox hits </h2>
         <p className='info'>
-          <a className='author' href="#">David Paztho</a>
+          <a className='author'>David Paztho</a>
           <time>2023-02-22 11.40</time>
         </p>
         <p className='summary'>EV charging has improved over the years, of electric vehicles the Biden Administration wants to see on the road in the next . $7.5 billion EV charging.</p>
