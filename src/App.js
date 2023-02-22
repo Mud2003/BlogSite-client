dd6dad8773b5f5388753df793d46d6e706250387
Muditha Madusith <108977790+Mud2003@users.noreply.github.com>
2023-02-22 12:35:00 +0530

    Add Routes
    Adding route with react-router-.

--- src/App.js
+++ src/App.js
@@ -1,60 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Layout from './components/Layout';
+import IndexPage from './pages/IndexPage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
-    <main>
-
-      <header>
-        <a href="" className="logo">MyBlog</a>
-        <nav>
-          <a href="">Login</a>
-          <a href="">Register</a>
-        </nav>
-      </header>
-
-      <div className="post">
-        <div className="image">
-        <img src="https://techcrunch.com/wp-content/uploads/2023/02/Zoox-making-right-hand-turn-on-open-public-roads-in-Foster-City-California.jpg?w=990&crop=1" alt=""/>
-        </div>
-        <div className="texts">
-        <h2>Tesla battles with union organizers, Zoox hits </h2>
-        <p className='info'>
-          <a className='author'>David Paztho</a>
-          <time>2023-02-22 11.40</time>
-        </p>
-        <p className='summary'>EV charging has improved over the years, of electric vehicles the Biden Administration wants to see on the road in the next . $7.5 billion EV charging.</p>
-        </div>
-      </div>
-      
-      <div className="post">
-        <div className="image">
-        <img src="https://techcrunch.com/wp-content/uploads/2023/02/Zoox-making-right-hand-turn-on-open-public-roads-in-Foster-City-California.jpg?w=990&crop=1" alt=""/>
-        </div>
-        <div className="texts">
-        <h2>Tesla battles with union organizers, Zoox hits </h2>
-        <p className='info'>
-          <a className='author'>David Paztho</a>
-          <time>2023-02-22 11.40</time>
-        </p>
-        <p className='summary'>EV charging has improved over the years, of electric vehicles the Biden Administration wants to see on the road in the next . $7.5 billion EV charging.</p>
-        </div>
-      </div>
-
-      <div className="post">
-        <div className="image">
-        <img src="https://techcrunch.com/wp-content/uploads/2023/02/Zoox-making-right-hand-turn-on-open-public-roads-in-Foster-City-California.jpg?w=990&crop=1" alt=""/>
-        </div>
-        <div className="texts">
-        <h2>Tesla battles with union organizers, Zoox hits </h2>
-        <p className='info'>
-          <a className='author'>David Paztho</a>
-          <time>2023-02-22 11.40</time>
-        </p>
-        <p className='summary'>EV charging has improved over the years, of electric vehicles the Biden Administration wants to see on the road in the next . $7.5 billion EV charging.</p>
-        </div>
-      </div>
-
-    </main>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<IndexPage />} />
+        <Route path="/login" element={<LoginPage/>} />
+      </Route>
+    </Routes>
   );
 }
 
