c707c516e4c23273ee5d270e1c3830bb146fe9aa
Muditha Madusith <108977790+Mud2003@users.noreply.github.com>
2023-02-22 14:45:04 +0530

    Update Post.js

--- src/components/Post.js
+++ src/components/Post.js
@@ -7,7 +7,7 @@ export default function Post() {
         <div className="texts">
         <h2>Tesla battles with union organizers, Zoox hits </h2>
         <p className='info'>
-          <a className='author'>David Paztho</a>
+          <a className='author' href="#">David Paztho</a>
           <time>2023-02-22 11.40</time>
         </p>
         <p className='summary'>EV charging has improved over the years, of electric vehicles the Biden Administration wants to see on the road in the next . $7.5 billion EV charging.</p>
