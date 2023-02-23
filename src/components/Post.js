d4c7eaa147e7c4036550b088e1e50a46076ba570
Muditha Madusith <108977790+Mud2003@users.noreply.github.com>
2023-02-23 20:39:46 +0530

    Display Posts from DB

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,17 +1,25 @@
-export default function Post() {
-    return (
-        <div className="post">
-        <div className="image">
-        <img src="https://techcrunch.com/wp-content/uploads/2023/02/Zoox-making-right-hand-turn-on-open-public-roads-in-Foster-City-California.jpg?w=990&crop=1" alt=""/>
-        </div>
-        <div className="texts">
-        <h2>Tesla battles with union organizers, Zoox hits </h2>
-        <p className='info'>
-          <a className='author'>David Paztho</a>
-          <time>2023-02-22 11.40</time>
-        </p>
-        <p className='summary'>EV charging has improved over the years, of electric vehicles the Biden Administration wants to see on the road in the next . $7.5 billion EV charging.</p>
-        </div>
+import {formatISO9075} from "date-fns";
+import {Link} from "react-router-dom";
+
+export default function Post({_id,title,summary,cover,createdAt,author}) {
+
+  return (
+    <div className="post">
+      <div className="image"> 
+        <Link to={`/post/${_id}`}>
+          <img src={'http://localhost:4000/'+cover} alt=""/>
+        </Link>
       </div>
-    );
+      <div className="texts">
+        {/*<Link to={`/post/${_id}`}>*/}
+        <h2>{title}</h2>
+        {/*</Link>*/}
+        <p className="info">
+          <a className="author">{author.username}</a>
+          <time>{formatISO9075(new Date(createdAt))}</time>
+        </p>
+        <p className="summary">{summary}</p>
+      </div>
+    </div>
+  );
 }
